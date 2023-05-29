8e4a0f457ac0f1416849e23e1686b5f390de9c98
zhongdongbiao <zhongdongbiao@utry.cn>
2023-05-29 11:03:12 +0800

    测试git

--- src/main/java/utry/data/modular/partsManagement/controller/CoreIndexController.java
+++ src/main/java/utry/data/modular/partsManagement/controller/CoreIndexController.java
@@ -96,6 +96,7 @@ public class CoreIndexController extends CommonController {
     public RetResult getAllBear() {
         List<HrmAccountInfoDTO> hrmAccountInfoDTOS = userFactoryDao.selectConfig();
         System.out.println("123");
+        System.out.println("123");
         return RetResponse.makeOKRsp(hrmAccountInfoDTOS);
     }
 
